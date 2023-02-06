8dc09ed33f272664b5bad0a19e50519ba9473e3c
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-02-07 01:21:55 +0300

    57. Robot Return to Origin solved

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        FlippingAnImage fi = new FlippingAnImage();
-        fi.solution();
+        RobotReturnToOrigin rr = new RobotReturnToOrigin();
+        rr.solution();
     }
 }
