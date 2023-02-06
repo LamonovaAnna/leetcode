1e7c01963c7fcd1f122f0948d0e0d528d6558181
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-02-06 21:37:02 +0300

    804. Unique Morse Code Words solved

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        JewelsAndStones js = new JewelsAndStones();
-        js.solution();
+        UniqueMorseCodeWords um = new UniqueMorseCodeWords();
+        um.solution();
     }
 }
