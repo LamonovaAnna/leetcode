b61c313b2af1e29cf332e6f859930d4e78e3e56f
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-02-07 18:14:55 +0300

    42. Power of Four solved

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        PowerOfTwo pt = new PowerOfTwo();
-        pt.solution();
+        PowerOfFour pf = new PowerOfFour();
+        pf.solution();
     }
 }
