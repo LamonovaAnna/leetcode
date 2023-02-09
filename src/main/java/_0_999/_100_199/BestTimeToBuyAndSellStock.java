28776119e298d35fe9c6d2bc708e0f6f96d3c60a
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-02-09 16:13:07 +0300

    Best Time to Buy and Sell Stock solved

---
+++ src/main/java/_0_999/_100_199/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,27 @@
+package _0_999._100_199;
+
+//121. Best Time to Buy and Sell Stock
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+
+
+    public void solution() {
+        System.out.println(maxProfit(new int[]{1, 2, 4}));
+        System.out.println(maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+        System.out.println(maxProfit(new int[]{2, 2, 5}));
+        System.out.println(maxProfit(new int[]{3, 3}));
+
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+        System.out.println(maxProfit(new int[]{1, 2}));
+
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,9 @@
+import _0_999._100_199.BestTimeToBuyAndSellStock;
 import _0_999._200_299.FirstBadVersion;
 
 public class main {
     public static void main(String[] args) {
-        FirstBadVersion t = new FirstBadVersion();
-        t.solution();
+        BestTimeToBuyAndSellStock bt = new BestTimeToBuyAndSellStock();
+        bt.solution();
     }
 }
