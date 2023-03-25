b8e596b139882bcd1c44331a4a2fdb94697e6b90
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-03-25 18:04:54 +0100

    1662. Check If Two String Arrays are Equivalent solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,9 @@
-import _1000_1999._1800_1899._1859_SortingTheSentence;
+import _1000_1999._1600_1699._1662_CheckIfTwoStringArraysAreEquivalent;
 
 public class Solution {
     public static void main(String[] args) {
-        _1859_SortingTheSentence ss = new _1859_SortingTheSentence();
-        ss.solution();
+        _1662_CheckIfTwoStringArraysAreEquivalent ct = new _1662_CheckIfTwoStringArraysAreEquivalent();
+        ct.solution();
     }
 
 
---
+++ src/main/java/_1000_1999/_1600_1699/_1662_CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,23 @@
+package _1000_1999._1600_1699;
+
+// 1662. Check If Two String Arrays are Equivalent
+public class _1662_CheckIfTwoStringArraysAreEquivalent {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String s : word1) {
+            sb1.append(s);
+        }
+        for (String s : word2) {
+            sb2.append(s);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
+
+    public void solution() {
+        System.out.println(arrayStringsAreEqual(new String[]{"ab", "c"}, new String[]{"a", "bc"}));
+        System.out.println(arrayStringsAreEqual(new String[]{"a", "cb"}, new String[]{"ab", "c"}));
+        System.out.println(arrayStringsAreEqual(new String[]{"abc", "d", "defg"}, new String[]{"abcddefg"}));
+
+    }
+}
