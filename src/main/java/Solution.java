150b6fa988ca951baebd1cc3969ecd5d16600141
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-03-31 00:00:14 +0200

    283. Move Zeroes solved better

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,9 @@
-import _1000_1999._1500_1599._1512_NumberOfGoodPairs;
+import _0_999._200_299._283_MoveZeroes;
 
 public class Solution {
     public static void main(String[] args) {
-        _1512_NumberOfGoodPairs no = new _1512_NumberOfGoodPairs();
-        no.solution();
+        _283_MoveZeroes mz = new _283_MoveZeroes();
+        mz.solution();
     }
 
 
--- src/main/java/_0_999/_200_299/_283_MoveZeroes.java
+++ src/main/java/_0_999/_200_299/_283_MoveZeroes.java
@@ -3,17 +3,22 @@ package _0_999._200_299;
 //283. Move Zeroes
 public class _283_MoveZeroes {
     public void moveZeroes(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] != 0) {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                        break;
-                    }
-                }
+        if (nums.length == 1) {
+            return;
+        }
+
+        int counter = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[counter] = num;
+                counter++;
             }
         }
+
+        for (int i = counter; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
     }
 
 
