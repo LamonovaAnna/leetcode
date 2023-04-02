74d97e396801a603c6efa024a21f4a613652d24f
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-04-02 03:10:54 +0200

    2501. Longest Square Streak in an Array solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,9 @@
-import _2000_2999._2500_2599._2500_DeleteGreatestValueInEachRow;
+import _2000_2999._2500_2599._2501_LongestSquareStreakInAnArray;
 
 public class Solution {
     public static void main(String[] args) {
-        _2500_DeleteGreatestValueInEachRow dg = new _2500_DeleteGreatestValueInEachRow();
-        dg.solution();
+        _2501_LongestSquareStreakInAnArray ls = new _2501_LongestSquareStreakInAnArray();
+        ls.solution();
     }
 
 
