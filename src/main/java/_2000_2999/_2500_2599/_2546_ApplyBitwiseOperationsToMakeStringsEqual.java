8a1d61c7d4c379f281833dbc2963ddb2af7274bc
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-04-03 00:27:39 +0200

    2546. Apply Bitwise Operations to Make Strings Equal solved

---
+++ src/main/java/_2000_2999/_2500_2599/_2546_ApplyBitwiseOperationsToMakeStringsEqual.java
@@ -0,0 +1,10 @@
+package _2000_2999._2500_2599;
+
+
+// 2546. Apply Bitwise Operations to Make Strings Equal
+public class _2546_ApplyBitwiseOperationsToMakeStringsEqual {
+    public boolean makeStringsEqual(String s, String target) {
+        return s.contains("1") == target.contains("1");
+
+    }
+}
