4966c288ca57444d097b5212910996abb8aaf50f
AnnaLamonova <lamonova.a.v@yandex.ru>
2023-04-09 17:17:06 +0200

    1374. Generate a String With Characters That Have Odd Counts solved

---
+++ src/main/java/_1000_1999/_1300_1399/_1374_GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -0,0 +1,19 @@
+package _1000_1999._1300_1399;
+
+// 1374. Generate a String With Characters That Have Odd Counts
+public class _1374_GenerateAStringWithCharactersThatHaveOddCounts {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+        int length = 0;
+
+        sb.append("a".repeat(Math.max(0, n - 1)));
+
+        if (n % 2 == 0) {
+            sb.append('b');
+        } else {
+            sb.append('a');
+        }
+        return sb.toString();
+
+    }
+}
